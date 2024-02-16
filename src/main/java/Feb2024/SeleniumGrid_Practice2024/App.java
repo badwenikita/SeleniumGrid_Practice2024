2d38e15c55340f8be3968a03800d413d2382e98f
badwenikita <badwe.nikita@gmail.com>
2024-02-16 14:27:34 +0530

    First commit

---
+++ src/main/java/Feb2024/SeleniumGrid_Practice2024/App.java
@@ -0,0 +1,13 @@
+package Feb2024.SeleniumGrid_Practice2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Feb2024/SeleniumGrid_Practice2024/SeleniumGridPractice.java
@@ -0,0 +1,60 @@
+package Feb2024.SeleniumGrid_Practice2024;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+
+public class SeleniumGridPractice {
+	
+	WebDriver driver;
+	ChromeOptions co;
+	
+	@BeforeTest
+	public void setup()
+	{
+		/*
+		co = new ChromeOptions();
+		co.setCapability("enableVNC", true);	// to visualize the executing TCs in the containers we pass this capability Name. 
+		co.setPlatformName("linux");
+		try {
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), co);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		*/
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	public void titleTest()
+	{
+		String actual = driver.getTitle();
+		Assert.assertEquals(actual, "Google");
+		
+	}
+	
+	@Test
+	public void urlTest()
+	{
+		String actual = driver.getCurrentUrl();
+		Assert.assertEquals(actual, "https://www.google.com/");
+	}
+	
+	
+	
+	
+	
+	
+
+}
